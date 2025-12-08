01c8e41b51f5d92b2308e6be3a5b8b579dd188c7
AbHXn <asofficial47@gmail.com>
2025-12-08 20:26:02 +0530

    latest

--- lang.cpp
+++ lang.cpp
@@ -1,45 +1,161 @@
 #include <iostream>
 #include <variant>
+#include <memory>
+#include <queue>
+#include <unordered_map>
+#include <string>
+#include <optional>
+
 using namespace std;
 
-enum class DTYPES{
-	VALUE_NOT_DEFINED, STRING, INT, DOUBLE, BOOLEAN
+using VarDtype = variant<string, long int, double, bool>;
+
+
+enum class DTYPES
+{
+	VALUE_NOT_DEFINED, 
+	STRING 			 , 
+	INT 			 , 
+	DOUBLE 			 , 
+	BOOLEAN
 };
+
+enum class VALUE_TOKENS
+{
+	ARRAY_OPEN 	,
+	ARRAY_VALUE ,
+	NORMAL_VALUE,
+	COMMA 		,
+	ARRAY_CLOSE ,
+	VALUE_END
+};
+
+enum class VARIABLE_TOKENS
+{
+	VAR_START 	 ,
+	NAME 	 	 ,
+	COMMA 		 ,
+	ARRAY 		 ,
+	VALUE_ASSIGN ,
+	VAR_ENDS	
+};
+
+queue<string> VarQueue, ValueQueue;
+
+// variable syntax handling...
+unordered_map <VARIABLE_TOKENS, vector<VARIABLE_TOKENS>> VARIABLE_DECLARE_GRAPH = {
+	{ VARIABLE_TOKENS::VAR_START, 	{ VARIABLE_TOKENS::NAME } 								  },
+	{ VARIABLE_TOKENS::NAME,		{ VARIABLE_TOKENS::COMMA, VARIABLE_TOKENS::VALUE_ASSIGN, 
+					  				  VARIABLE_TOKENS::ARRAY, VARIABLE_TOKENS::VAR_ENDS }     },
+	{ VARIABLE_TOKENS::ARRAY, 		{ VARIABLE_TOKENS::COMMA, VARIABLE_TOKENS::VALUE_ASSIGN, 
+					  				  VARIABLE_TOKENS::VAR_ENDS } 		 					  },
+	{ VARIABLE_TOKENS::COMMA, 		{ VARIABLE_TOKENS::NAME } 						  		  }	
+};
+
+// value syntax handling...
+unordered_map <VALUE_TOKENS, vector<VALUE_TOKENS>> VALUE_ASSIGN_GRAPH = {
+	{ VALUE_TOKENS::NORMAL_VALUE, { VALUE_TOKENS::COMMA, VALUE_TOKENS::VALUE_END } 			},
+	{ VALUE_TOKENS::COMMA, 		  { VALUE_TOKENS::NORMAL_VALUE }							}, 
+	// ARRAY CASE
+	{ VALUE_TOKENS::ARRAY_OPEN,  { VALUE_TOKENS::ARRAY_OPEN, VALUE_TOKENS::ARRAY_VALUE } 	},
+	{ VALUE_TOKENS::ARRAY_VALUE, { VALUE_TOKENS::COMMA, VALUE_TOKENS::ARRAY_CLOSE } 	   	},
+	{ VALUE_TOKENS::COMMA, 	  	 { VALUE_TOKENS::ARRAY_VALUE, VALUE_TOKENS::ARRAY_OPEN } 	},
+	{ VALUE_TOKENS::ARRAY_CLOSE, { VALUE_TOKENS::COMMA, VALUE_TOKENS::VALUE_END }		   	}
+};
+
+
+pair<vector<VARIABLE_TOKENS>, vector<VALUE_TOKENS>> codeToTokens( vector<string>& tokens, size_t& curIndexPtr ){
+	int startCurPtr = curIndexPtr;
+
+	vector<VARIABLE_TOKENS> varTokens;
+	vector<VALUE_TOKENS> 	valueToken;
+
+	bool isVariableTurn = true;
+
+	for( ; startCurPtr < tokens.size(); ++startCurPtr ) {
+		string curToken = tokens[ startCurPtr ];
+
+		if( curToken == "pidi" ){
+			varTokens.push_back( VARIABLE_TOKENS::VAR_START );
+		}
+		
+		else if( curToken == ","){
+			( isVariableTurn ) ? 
+				varTokens.push_back( VARIABLE_TOKENS::COMMA )
+				: valueToken.push_back( VALUE_TOKENS::COMMA );
+		}
+		
+		else if( curToken == "kootam" ){
+			varTokens.push_back( VARIABLE_TOKENS::ARRAY );
+		}
+
+		else if ( curToken == "{" ){
+			valueToken.push_back( VALUE_TOKENS::ARRAY_OPEN );
+		}
+		
+		else if( curToken == "="){
+			varTokens.push_back( VARIABLE_TOKENS::VALUE_ASSIGN );
+			isVariableTurn = false;
+		}
+
+		else if(curToken == "}"){
+			valueToken.push_back( VALUE_TOKENS::ARRAY_CLOSE );
+		}
+
+		else if(curToken == ";"){
+			( isVariableTurn ) ? 
+				varTokens.push_back( VARIABLE_TOKENS::VAR_ENDS )
+				: valueToken.push_back( VALUE_TOKENS::VALUE_END );
+		}
+		else{
+			if( isVariableTurn ){
+				VarQueue.push( tokens[ startCurPtr ] );
+				varTokens.push_back( VARIABLE_TOKENS::NAME );
+			}else{
+				ValueQueue.push( tokens[ startCurPtr ] );
+				valueToken.push_back( VALUE_TOKENS:: )
+			}
+		}		
+	}
+	return { varTokens, valueToken };
+}
+
 
 class SingleElement{
 	private:
 		bool isValueDefined = false;
-		using VarDtype = variant<string, long int, double, bool>;
 		VarDtype data;
 	
 	public:
-		DTYPES getValueDtypes( void ) const {
+		DTYPES getValueDtypes( void ) 
+		const {
 			if( !isValueDefined )
 				return DTYPES::VALUE_NOT_DEFINED;
-
-			else if( hold_alternative<string>( this->data ) )
+			
+			else if( holds_alternative<string>( this->data ) )
 				return DTYPES::STRING;
 
-			else if( hold_alternative<long int> ( this->data ) )
+			else if( holds_alternative<long int> ( this->data ) )
 				return DTYPES::INT;
 
-			else if( hold_alternative<double> ( this->data ) )
+			else if( holds_alternative<double> ( this->data ) )
 				return DTYPES::DOUBLE;
 
 			else return DTYPES::BOOLEAN;
-
 		}
 
-		void assignValue(const VarDtype& value ){
+		void assignValue(const VarDtype& value )
+		{
 			this->data 	= value;
 			this->isValueDefined = true;
 		}
-
+		
 		template <typename Dtype>
-		optional<Dtype> getValue( void ){
+		optional<Dtype> getValue( void ) 
+		const {
 			DTYPES realType = this->getValueDtypes( );
 			
-			if( realType == VALUE_NOT_DEFINED )
+			if( realType == DTYPES::VALUE_NOT_DEFINED )
 				return nullopt;
 
 			if( auto data = get_if<Dtype>( &this->data ) )
@@ -47,19 +163,73 @@ class SingleElement{
 
 			return nullopt;
 		}
-}
+};
 
-template <typename Dtype>
-class Array{
+class ArrayList{
 	private:
-		size_t elementSize;
-		vector<ArrayElement<Dtype>> dims;
+		variant<vector<SingleElement>, vector<ArrayList>> arrayList;
+		vector<size_t> dimensions;	
+		size_t totalElementsAllocated;
+
+		unique_ptr<ArrayList> _arrayListBuilder( vector<VALUE_TOKENS>& arrayTokenList, size_t curIndex, unordered_map<VALUE_TOKENS, queue<SingleElement>>& Values ) 
+		{
+			auto newArrayList = make_unique< ArrayList >();
+			vector<SingleElement> resultList;
+			
+			while( curIndex < arrayTokenList.size() ){
+				if( arrayTokenList[ curIndex ] == VALUE_TOKENS::COMMA )
+				{
+					continue;
+				}
+				else if( arrayTokenList[ curIndex ] == VALUE_TOKENS::ARRAY_OPEN )
+				{
+					unique_ptr<ArrayList> array = _arrayListBuilder( arrayTokenList, curIndex + 1, Values );
+					newArrayList->push_ArrayList( *array );
+				}
+				else if(
+				 	auto value = Values.find( arrayTokenList[ curIndex ] );
+				 	value != Values.end() 
+				 )
+				{
+					auto data = value->second.front( );
+					value->second.pop( );
+					resultList.push_back( data );
+				}
+				else if( arrayTokenList[ curIndex ] == VALUE_TOKENS::ARRAY_CLOSE )
+				{
+					newArrayList->push_SingleElement( resultList );
+					return newArrayList;
+				}
+			}
+			return newArrayList;
+
+		}
+
 	public:
+		void createArray( vector<VALUE_TOKENS>& arrayTokenList ){
+			unordered_map<VALUE_TOKENS, queue<SingleElement>> values;
+			_arrayListBuilder( arrayTokenList, 0,  values );	
+		}
 
-}
+		void push_SingleElement( vector<SingleElement>& singleElementList ){
+			arrayList = singleElementList;
+		}
 
-template <typename dtype>
-struct Variable{
-	string key;
-	variant<dtype, ArrayType<dtype>> data;
+		void push_ArrayList( ArrayList& arrayListElement ){
+			auto* vec = std::get_if<std::vector<ArrayList>>(&arrayList);
+
+		    if (!vec) return;
+		    vec->push_back(arrayListElement);
+		}
+
+};
+
+int main(){
+	vector<string> test =  { "pidi", "name", "=", "190", ";" };
+	size_t index = 0;
+	auto res = codeToTokens( test, index );
+	vector<VARIABLE_TOKENS>t = res.first;
+	for(int x = 0; x < t.size(); x++){
+		cout << (int)t[x] << endl;
+	}
 }
